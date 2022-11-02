8b349b91a5621219461627f2696eb61097a9ac62
lloydrichards <lloyd.d.richards@gmail.com>
2022-11-02 13:30:39 +0100

    feat(API): :sparkles: added search and details endpoints

---
+++ dashboard/config/prisma.tsx
@@ -0,0 +1,18 @@
+import { PrismaClient } from '@prisma/client';
+
+let prisma: PrismaClient;
+
+// NOTE: Prevent multiple instances of Prisma Client in development
+declare const global: Global & { prisma?: PrismaClient };
+
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export default prisma;
---
+++ dashboard/pages/api/search.ts
@@ -0,0 +1,43 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import * as E from 'fp-ts/Either';
+import { pipe } from 'fp-ts/lib/function';
+import * as t from 'io-ts';
+import { failure } from 'io-ts/lib/PathReporter';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../../config/prisma';
+
+const SearchParam = t.type({
+  query: t.string,
+});
+type SearchParam = t.TypeOf<typeof SearchParam>;
+
+type IData = {
+  err?: string;
+  data: any;
+};
+
+const handler = async (req: NextApiRequest, res: NextApiResponse<IData>) => {
+  await prisma.$connect();
+
+  return await pipe(
+    SearchParam.decode(req.body.data),
+    E.fold(
+      (l) => {
+        res.status(500).json({
+          err: `Can't parse this: ${req.body.data}`,
+          data: failure(l),
+        });
+      },
+      async (r) => {
+        const data = await prisma.tree.findFirst({
+          where: { genus: { contains: r.query } },
+        });
+        res.status(200).json({
+          data: 'Success',
+        });
+      }
+    )
+  );
+};
+
+export default handler;
